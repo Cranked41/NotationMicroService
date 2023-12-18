607644cf24f7d01d2aa8408d82a8ba8444e4d141
ALİ RIZA ÇELİK <aliriza.celik@dijitalkurye.com.tr>
2023-12-18 17:05:51 +0300

    first commit

---
+++ src/main/java/com/microservice/notation/NotationApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.notation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservice/notation/NotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.notation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
